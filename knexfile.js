34974017463778fce3ee185bae25bbd669a1e52a
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2018-02-01 12:59:07 -0500

    update migrations

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/db/migrations/production'
     },
     seeds: {
       directory: __dirname + '/db/seeds/production'
