70c1fadd0376d352631aff5a6e2fce817c1e1954
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2018-02-01 13:10:02 -0500

    Update ssl setting for knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://gddsnltcqcsnay:6b7bd5f78eab23f5f0384ff0903955d4c54e4dc2f9fa0b7a2296314177629ff7@ec2-50-16-231-2.compute-1.amazonaws.com:5432/dfb1s48to9ente',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations/production'
     },
