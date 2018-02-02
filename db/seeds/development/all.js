eede6c93b3a2531f3cc2b12123c71b19830ee748
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2018-02-02 15:07:31 -0600

    add seed

--- db/seeds/development/all.js
+++ db/seeds/development/all.js
@@ -1,7 +1,6 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return Promise.join(
-    knex('utxo').del(),
-    knex('user_profile').del(),
+    knex('companies').insert({secret_phrase: 'ul2017', name: 'Underwriters Laboratories'}),
     () => {});
 };
---
+++ db/seeds/production/all.js
@@ -0,0 +1,6 @@
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return Promise.join(
+      knex('companies').insert({secret_phrase: 'ul2017', name: 'Underwriters Laboratories'}),
+      () => {});
+  };
